bbc3c0cd81a3372d276bc8ef3c1416c405aa7391
nyaoath_mac <nyapath@163.com>
2018-08-03 17:16:04 +0800

    “cccc”

---
+++ learnC/exm1/exm1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*这是内容 */
+int main (int arhc,char * argv[])
+{
+	int distance=100;
+	int age=10;
+	//这也是内容
+	printf("You are %d miles away. \n",distance);
+	
+	printf("小明今年%d 岁了. \n",age);
+	return 0;
+}
